faada9689aadae1f6be371f40d92bf137352fce9
Sergei Primosudov <sergei.primosudov@netcracker.com>
2023-05-09 18:37:53 +0400

    f-22 refactor

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,20 +3,6 @@ import { Helmet } from 'react-helmet-async';
 import { Store } from '../Store';
 import { Card, ListGroup } from 'react-bootstrap';
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'UPDATE_REQUEST':
-            return { ...state, loadingUpdate: true };
-        case 'UPDATE_SUCCESS':
-            return { ...state, loadingUpdate: false };
-        case 'UPDATE_FAIL':
-            return { ...state, loadingUpdate: false };
-
-        default:
-            return state;
-    }
-};
-
 export default function ProfileScreen() {
     const { state, dispatch: ctxDispatch } = useContext(Store);
     const { userInfo } = state;
